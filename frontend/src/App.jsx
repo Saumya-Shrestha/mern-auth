b9423558ca19e040e2287bf0e4119ab789d2ada8
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-03-29 21:21:21 +0545

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <RouteHandler />
+      </BrowserRouter>
+    </>
+  );
+};
+
+const RouteHandler = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="signUp" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="container w-screen h-screen flex items-center justify-center flex-col">
+      <form className="w-[24vw] h-[40vh] flex flex-col bg-[#0a0a0a] p-[20px] rounded-xl shadow-xl shadow-black/50">
+        <h3 className="text-2xl">SignUp</h3>
+
+        <div className="inputBox">
+          <input type="text" placeholder="Full Name" required />
+        </div>
+
+        <div className="inputBox">
+          <input type="email" placeholder="Email" required />
+        </div>
+
+        <div className="inputBox">
+          <input type="password" placeholder="Password" required />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
