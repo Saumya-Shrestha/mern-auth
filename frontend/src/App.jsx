9577762fa1e7d0190a4f3a35c17f1423c6083236
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-05-11 12:36:01 +0545

    Completed auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import { api_base_url } from './helper';
 
 const App = () => {
   return (
@@ -12,20 +13,46 @@ const App = () => {
         <RouteHandler />
       </BrowserRouter>
     </>
-  );
+  )
 };
 
 const RouteHandler = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="signUp" element={<SignUp />} />
-        <Route path="login" element={<Login />} />
-        [v4.7]
-      </Routes>
-    </>
-  );
-};
+  let location = useLocation();
+  const navigate = useNavigate();
 
-export default App;
+  const authenticateUser = () => {
+    fetch(api_base_url + "/authencateUser", {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        token: localStorage.getItem("token")
+      })
+    }).then(res => res.json()).then(data => {
+      if (data.success === false) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      }
+    })
+  }
+
+  useEffect(() => {
+    // console.log(location.pathname);
+    if (location.pathname !== '/signUp' && location.pathname !== '/login') {
+      authenticateUser();
+    }
+  }, [location])
+
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signUp' element={<SignUp />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
+  )
+}
+
+export default App
